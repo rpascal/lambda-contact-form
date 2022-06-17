b40a51d09233703073322d8d11927b8e5e133ee6
Ryan Pascal <ryan.david.pascal@gmail.com>
2022-06-17 14:19:22 -0400

    Setting up some gradle imports

---
+++ src/main/java/com/coldbrewcode/contact/LambdaContactFormBootstrap.java
@@ -0,0 +1,9 @@
+package com.coldbrewcode.contact;
+
+public class LambdaContactFormBootstrap {
+
+    public static void main(String[] arg) {
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/main/java/com/coldbrewcode/contact/LambdaContactFormMockServerApplication.java
@@ -0,0 +1,11 @@
+package com.coldbrewcode.contact;
+
+import com.coldbrewcode.aws.lambda.mock.MockLambdaApiRequestProcessor;
+
+public class LambdaContactFormMockServerApplication {
+
+    public static void main(String[] args) {
+//        new MockLambdaApiRequestProcessor().startServer(4578, handler::handleRequest);
+    }
+
+}
